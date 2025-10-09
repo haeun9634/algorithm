c3eae8e756976bc2a1147b56dbb0f0d3a7115778
haeun <103066456+haeun9634@users.noreply.github.com>
2025-10-09 20:53:54 +0900

    [level 1] Title: 실패율, Time: 3.08 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.cpp
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+
 using namespace std;
+
 bool compare(pair<int,double>& a, pair<int,double>& b){
-    if(a.second==b.second)//같을 경우
-        return a.first < b.first;//작은 번호 먼저, 오름차순
-    return a.second > b.second;
+    if(a.second==b.second)
+        return a.first<b.first;//오름차순
+    return a.second>b.second;//내림차순
 }
 
 vector<int> solution(int N, vector<int> stages) {
@@ -15,24 +17,23 @@ vector<int> solution(int N, vector<int> stages) {
     for(int i=0; i<stages.size(); i++){
         mp[stages[i]]++;
     }
-    vector<pair<int,double>> st;
-    int total = stages.size();
+    vector <pair<int, double>> failrate;
+    int temp = stages.size();
     for(int i=1; i<=N; i++){
-        double failRate = 0.0;
-
-        if (total != 0) { // 분모가 0이 아닐 때만 계산
-            failRate = (double)mp[i] / total;
-        }
-
-        st.push_back({i, failRate});
-        total -= mp[i];
+        double num=0;
+        if(temp!=0)
+            num =(double)mp[i]/temp;
+        failrate.push_back({i,num});
+        temp -= mp[i];
+    }
+    sort(failrate.begin(), failrate.end(), compare);
+    
+    for(int i=0; i<N; i++){
+        answer.push_back(failrate[i].first);
     }
     
-    sort(st.begin(), st.end(), compare);
-    for(int i=0; i<st.size(); i++){
-        answer.push_back(st[i].first);
-    }
+
     
-        
+    
     return answer;
 }
